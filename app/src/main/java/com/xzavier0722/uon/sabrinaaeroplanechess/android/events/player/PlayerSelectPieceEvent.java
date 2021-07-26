419b9c89a19b3f002c083f275f359309af04ae18
Xzavier0722 <Xzavier0722@gmail.com>
2021-07-27 02:14:35 +0800

    Add getAvailablePieces for PlayerSelectPieceEvent

--- app/src/main/java/com/xzavier0722/uon/sabrinaaeroplanechess/android/events/player/PlayerSelectPieceEvent.java
+++ app/src/main/java/com/xzavier0722/uon/sabrinaaeroplanechess/android/events/player/PlayerSelectPieceEvent.java
@@ -4,19 +4,30 @@ import com.xzavier0722.uon.sabrinaaeroplanechess.android.core.Player;
 import com.xzavier0722.uon.sabrinaaeroplanechess.android.core.chess.ChessBoard;
 import com.xzavier0722.uon.sabrinaaeroplanechess.android.core.chess.Piece;
 
+import java.util.Set;
+
 public class PlayerSelectPieceEvent extends PlayerDiceEvent {
 
     private Piece selectedPiece;
+    private final Set<Piece> availablePieces;
 
-    public PlayerSelectPieceEvent(ChessBoard chessBoard, Player player, int diceNum) {
+    public PlayerSelectPieceEvent(ChessBoard chessBoard, Player player, int diceNum, Set<Piece> availablePieces) {
         super(chessBoard, player, diceNum);
+        this.availablePieces = availablePieces;
     }
 
-    public void setSelectedPieceId(Piece selectedPiece) {
+    public void setSelectedPiece(Piece selectedPiece) {
+        if (!availablePieces.contains(selectedPiece)) {
+            throw new IllegalArgumentException("The selected piece must in the available set");
+        }
         this.selectedPiece = selectedPiece;
     }
 
-    public Piece getSelectedPieceId() {
+    public Piece getSelectedPiece() {
         return selectedPiece;
+    }
+
+    public Set<Piece> getAvailablePieces() {
+        return availablePieces;
     }
 }
