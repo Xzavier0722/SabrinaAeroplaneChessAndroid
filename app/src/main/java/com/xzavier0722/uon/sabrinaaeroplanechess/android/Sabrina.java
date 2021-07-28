13f96e449d796b5c89a94216db8cc0245d88570a
Xzavier0722 <Xzavier0722@gmail.com>
2021-07-28 17:14:04 +0800

    Register listeners

--- app/src/main/java/com/xzavier0722/uon/sabrinaaeroplanechess/android/Sabrina.java
+++ app/src/main/java/com/xzavier0722/uon/sabrinaaeroplanechess/android/Sabrina.java
@@ -1,10 +1,17 @@
 package com.xzavier0722.uon.sabrinaaeroplanechess.android;
 
 import com.xzavier0722.uon.sabrinaaeroplanechess.android.core.event.EventManager;
+import com.xzavier0722.uon.sabrinaaeroplanechess.android.listeners.PieceListener;
+import com.xzavier0722.uon.sabrinaaeroplanechess.android.listeners.RobotListener;
 
 public class Sabrina {
 
     private static final EventManager eventManager = new EventManager();
+
+    static {
+        new PieceListener();
+        new RobotListener();
+    }
 
     public static EventManager getEventManager() {
         return eventManager;
